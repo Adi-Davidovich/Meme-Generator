c844bf2320aaf332635add46982c0c745279d13a
Adi-Davidovich <adidaflower@gmail.com>
2021-10-30 20:56:28 +0300

    refactor

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 'use strict'
 
-function toggleMenu() {
-    document.body.classList.toggle('menu-open');
+function openMenu() {
+    document.body.classList.add('menu-open');
+}
+function closeMenu() {
+    document.body.classList.remove('menu-open');
 }
